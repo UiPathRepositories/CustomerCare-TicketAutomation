57a75ce07d7fb2fc4710bff392033d34622b9b3f
Jeet <jeet.doshi476@gmail.com>
2020-01-04 18:42:03 +0530

    Base working code..

--- Hackathon-2019/MailService/UipathRun.bat
+++ Hackathon-2019/MailService/UipathRun.bat
@@ -1,1 +1,1 @@
-"C:\Users\Jeet14216\AppData\Local\UiPath\app-19.12.0-beta0061\UiRobot.exe" -file "D:\Jeet\UIPath sample\helloWorld\Main.xaml"
+"C:\Users\Jeet14216\AppData\Local\UiPath\app-19.12.0-beta0061\UiRobot.exe" -file "D:\Jeet\UIPath sample\Hackathon\Git\Hackathon-2019\Sentiment-Analyser\Main.xaml"
